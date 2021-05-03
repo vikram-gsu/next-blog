ea6d84458a9f962ca074d51f8c18b30e8e88cb61
Vikram Pareddy <vikram.pareddy@gmail.com>
2021-05-03 16:14:46 -0400

    Added grid layout to default layout

--- _includes/header.js
+++ _includes/header.js
@@ -1,7 +1,11 @@
-export default function Header(){
-    return <header>
-        <p>
-            Blog | Powered by Next JS
-        </p>
-    </header>
+import styled from "styled-components";
+const HeaderStyles = styled.header`
+  grid-area: header;
+`;
+export default function Header() {
+  return (
+    <HeaderStyles>
+      <p>Blog | Powered by Next JS</p>
+    </HeaderStyles>
+  );
 }
